55d8db9aea98c47ae40959847b4a1dcd99b4a764
HP-Harshit <harshitpatel274@gmail.com>
2025-03-04 15:02:44 +0530

    [Harshit] Added RegEx Pattern for Pin Code UC2

---
+++ Patterns/Pin Code/pincode UC3.js
@@ -0,0 +1,14 @@
+const pinCodePattern = /^[1-9][0-9]{5}$/;
+const pinCode1 = "400088";
+const pinCode2 = "A400088";
+const pinCode3 = "400088B";
+
+console.log(
+  pinCodePattern.test(pinCode1) ? "Valid PIN code" : "Invalid PIN code"
+); 
+console.log(
+  pinCodePattern.test(pinCode2) ? "Valid PIN code" : "Invalid PIN code"
+); 
+console.log(
+  pinCodePattern.test(pinCode3) ? "Valid PIN code" : "Invalid PIN code"
+); 
